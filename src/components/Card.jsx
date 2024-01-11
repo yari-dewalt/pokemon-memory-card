67de9712ed1ec5352a2caa9cbecb96edd09845b0
yari-dewalt <yari.dewalt@gmail.com>
2024-01-10 16:46:56 -0800

    Add prop for checking if any card is clicked and animate class

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,17 @@
+import React from 'react';
 import "../styles/Card.css";
 
-function Card({ pokemonData, onClick }) {
+function Card({ pokemonData, onClick, isAnyCardClicked }) {
   return (
-    <div className="card" id={pokemonData.key} onClick={onClick}>
-      <img className="card-image" src={pokemonData.pokemonImg}></img>
-      <h2 className="card-name">{pokemonData.pokemonName}</h2>
+    <div
+      className={"card"}
+      id={pokemonData.key}
+      onClick={onClick}
+    >
+      <div className={`card-content ${isAnyCardClicked ? "animate" : ""}`}>
+        <img className={"card-image"} src={pokemonData.pokemonImg} alt={pokemonData.pokemonName} />
+        <h2 className={"card-name"}>{pokemonData.pokemonName}</h2>
+      </div>
     </div>
   );
 }
