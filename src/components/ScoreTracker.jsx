9235fef55a079d895fca9e93a614737071d0343c
yari-dewalt <yari.dewalt@gmail.com>
2024-01-10 19:35:00 -0800

    Restructure to add support for trophy icon

--- src/components/ScoreTracker.jsx
+++ src/components/ScoreTracker.jsx
@@ -1,11 +1,16 @@
 import "../styles/ScoreTracker.css";
+import trophy from "../assets/trophy.png";
 
 function ScoreTracker({ currentScore, highScore })
 {
   return (
     <div className="score-tracker">
-      <h3 id="current-score">{`Current Score: ${currentScore}`}</h3>
-      <h3 id="high-score">{`High Score: ${highScore}`}</h3>
+      <h3 id="current-score">{`SCORE: ${currentScore}`}</h3>
+      <div className="high-score-container">
+        <h3 id="high-score-text">HIGH SCORE: </h3>
+        <img id="trophy" alt="trophy icon" src={trophy}></img>
+        <h3 id="high-score">{highScore}</h3>
+      </div>
     </div>
   )
 }
